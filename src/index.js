fca0308a4077a4ddb208f60f6a1904a413af76c2
Jan Razny <netorg@gmx.de>
2022-10-14 22:08:54 +0200

    appaointments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './css/index.css';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/umd/popper.js';
+import 'bootstrap/dist/js/bootstrap.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
